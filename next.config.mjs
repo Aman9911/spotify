765d5022d67071c16c3aa3438293d4a5553030d2
Aman9911 <aman.vdpl@gmail.com>
2024-03-31 22:45:49 +0530

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "oyseuwymcaieaouvdgzf.supabase.co",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
